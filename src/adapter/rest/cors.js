dd0e8c878bd0015410170c8db285deff38d0c731
navrotskyj <navrotskyj@gmail.com>
2016-02-22 15:59:08 +0200

    no message

--- src/adapter/rest/cors.js
+++ src/adapter/rest/cors.js
@@ -12,7 +12,7 @@ function addRoutes(api) {
         // CORS headers
         res.header("X-Powered-By", "Webitel");
         var origin = (req.headers.origin || "*");
-        res.header("Access-Control-Allow-Origin", origin); // restrict it to the required domain
+        res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,PATH,DELETE,OPTIONS');
 
         res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
