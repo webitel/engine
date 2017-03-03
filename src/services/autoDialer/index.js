ffebf0ba5d1925fcb42dcb0ba66788756315762c
Navrotskyj <navrotskyj@gmail.com>
2017-03-03 18:40:28 +0200

    dialer;  bug fix;

--- src/services/autoDialer/index.js
+++ src/services/autoDialer/index.js
@@ -242,7 +242,7 @@ class AutoDialer extends EventEmitter2 {
                 "args": {}
             }
              */
-            channel.bindQueue(qok.queue, application.Broker.Exchange.ENGINE, "*.dialer.systemm", {}, (e) => {
+            channel.bindQueue(qok.queue, application.Broker.Exchange.ENGINE, "*.dialer.system", {}, (e) => {
                 log.debug(`Init queue - successful`);
                 this.connectBroker = true;
                 this.emit('changeConnection');
@@ -332,7 +332,7 @@ class AutoDialer extends EventEmitter2 {
     }
 
     sendToBroker (data = {}, cb) {
-        application.Broker.publish(application.Broker.Exchange.ENGINE, `${encodeRK(application._instanceId)}.dialer.systemm`, data, e => {
+        application.Broker.publish(application.Broker.Exchange.ENGINE, `${encodeRK(application._instanceId)}.dialer.system`, data, e => {
             if (e)
                 log.error(e);
             return cb && cb(e);
