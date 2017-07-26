927d968d7640347ac0486b8000ffa0537f54bfd0
Navrotskyj <navrotskyj@gmail.com>
2017-07-26 13:26:02 +0300

    fix: ws variable variable_webitel_data

--- src/adapter/ws/handleEslEvent.js
+++ src/adapter/ws/handleEslEvent.js
@@ -53,10 +53,13 @@ function handleEslEvent(application) {
                             "variable_webitel_att_xfer": jsonEvent["variable_webitel_att_xfer"],
                             "variable_cc_queue": jsonEvent['variable_cc_queue'],
                             "variable_cc_member_session_uuid": jsonEvent['variable_cc_member_session_uuid'],
-                            "variable_webitel_data": "'" + jsonEvent['variable_webitel_data'] + "'",
+                            // "variable_webitel_data": "'" + jsonEvent['variable_webitel_data'] + "'",
                             "variable_w_transfer_result": jsonEvent['variable_w_transfer_result']
 
                         };
+                        if (jsonEvent.hasOwnProperty('variable_webitel_data'))
+                            jsonRequest.variable_webitel_data = "'" + jsonEvent['variable_webitel_data'] + "'"; //TODO
+
                         user.sendObject(jsonRequest);
                     }
                     ;
