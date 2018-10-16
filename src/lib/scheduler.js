59f0a919ecf77475e4a40f0cf877b00277424c70
navrotskyj <navrotskyj@gmail.com>
2018-10-16 15:16:41 +0300

    fixed: when scheduler long cron time

--- src/lib/scheduler.js
+++ src/lib/scheduler.js
@@ -9,33 +9,79 @@ const parser = require('cron-parser'),
     ;
 
 class Scheduler {
-    constructor (cronFormat, fn, params = {}) {
-        let _timer = null;
-        const interval = parser.parseExpression(cronFormat,  {tz: params.timezone});
-        const _c = cronFormat;
+    constructor(cronFormat, fn, options = {}) {
+        this.timerId = null;
+        this.name = fn.name ? fn.name : `unknown-${cronFormat}`;
+        this.isLog = !!options.log;
+        this.interval = parser.parseExpression(cronFormat,  {tz: options.timezone});
+        if (typeof fn === 'function') {
+            this._fn = fn;
+        } else {
+            throw `Bad constructor parameters fn`
+        }
 
-        log.info(`Create job: ${fn.name || ''} > ${cronFormat}`);
+        log.info(`Create job: ${this.name} -> ${cronFormat}`);
+        this.next();
+    }
 
-        this.cancel = () => clearTimeout(_timer);
+    next (intervalMs) {
+        if (!intervalMs)
+            intervalMs = this.getNextIntervalMs();
 
-        (function shed() {
-            if (_timer)
-                clearTimeout(_timer);
+        clearTimeout(this.timerId);
 
-            let n = -1;
-            do {
-                n = interval.next().getTime() - Date.now()
-            } while (n < 0);
-            if (params.log)
-                log.trace(`Next exec schedule: ${fn.name || ''} ${n}`);
+        if (intervalMs > 0x7FFFFFFF) {//setTimeout limit is MAX_INT32=(2^31-1)
+            log.debug(`job ${this.name}: sleep (divide segment ${msToHMS(intervalMs)})`);
+            this.timerId = setTimeout(() => {
+                this.next(intervalMs - 0x7FFFFFFF);
+            }, 0x7FFFFFFF);
+        } else {
+            if (this.isLog) {
+                log.debug(`jod ${this.name}: next schedule after ${msToHMS(intervalMs)} ms`);
+            }
+            this.timerId = setTimeout(() => {
+                if (this.isLog) {
+                    log.trace(`job ${this.name}: execute`);
+                }
+                this._fn.apply(null, [() => {
+                    this.next()
+                }]);
+            }, intervalMs);
+        }
+    }
 
-            _timer = setTimeout( function tick() {
-                if (params.log)
-                    log.trace(`Exec schedule: ${fn.name || ''} ${_c}`);
-                fn.apply(null, [shed]);
-            }, n);
-        })()
+    getNextIntervalMs () {
+        let n = -1;
+        let nextJob;
+        do {
+            nextJob = this.interval.next();
+            n = nextJob.getTime() - Date.now()
+        } while (n < 0);
+
+        return n;
+    }
+
+    cancel() {
+        clearTimeout(this.timerId);
+        log.debug(`job ${this.name} canceled`);
     }
 }
 
-module.exports = Scheduler;
+function pad(i) {
+    if (i < 10) {
+        return `0${i}`
+    } else {
+        return `${i}`
+    }
+}
+
+function msToHMS( ms ) {
+    let seconds = ms / 1000;
+    const hours = parseInt( seconds / 3600 );
+    seconds = seconds % 3600;
+    const minutes = parseInt( seconds / 60 );
+    seconds = seconds % 60;
+    return `${pad(hours)}:${pad(minutes)}:${pad(seconds)}`
+}
+
+module.exports = Scheduler ;
