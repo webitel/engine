e224d9187d26b5fe225ed25dadcaa126ac2aa6cd
Igor Navrotskyj <navrotskyj@gmail.com>
2023-05-25 11:37:19 +0300

    fixed rbac calls

--- store/sqlstore/call_store.go
+++ store/sqlstore/call_store.go
@@ -603,8 +603,8 @@ func (s SqlCallStore) GetHistoryByGroups(ctx context.Context, domainId int64, us
 		)
 	and (
 		(t.user_id = any (call_center.cc_calls_rbac_users(:Domain::int8, :UserSupervisorId::int8) || :Groups::int[])
-			or t.queue_id = any (call_center.cc_calls_rbac_queues(:ClassName::varchar, :Domain::int8, :UserSupervisorId::int8, :Groups::int[]))
-			or (t.user_ids notnull and t.user_ids::int[] && call_center.rbac_users_from_group(:Domain::int8, :Access::int2, :Groups::int[]))
+			or t.queue_id = any (call_center.cc_calls_rbac_queues(:Domain::int8, :UserSupervisorId::int8, :Groups::int[]))
+			or (t.user_ids notnull and t.user_ids::int[] && call_center.rbac_users_from_group(:ClassName::varchar, :Domain::int8, :Access::int2, :Groups::int[]))
 			or (t.grantee_id = any(:Groups::int[]))
 		)
 	)
