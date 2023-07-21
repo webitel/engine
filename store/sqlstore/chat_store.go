70067677d6bd5660fb50ec543b504b5a855e5477
Igor Navrotskyj <navrotskyj@gmail.com>
2023-07-21 12:34:49 +0300

    WTEL-3673

--- store/sqlstore/chat_store.go
+++ store/sqlstore/chat_store.go
@@ -36,13 +36,13 @@ select
     row_to_json(at) task,
 	at.leaving_at as leaving_at
 from (
-     select 1 pri, inv.created_at, null::varchar id, inv.id invite_id, null::timestamptz joined_at, inv.conversation_id, inv.user_id, inv.created_at updated_at, inv.props, null::timestamptz as closed_at
+     select 1 pri, inv.created_at, null::uuid id, inv.id::uuid invite_id, null::timestamptz joined_at, inv.conversation_id, inv.user_id, inv.created_at updated_at, inv.props, null::timestamptz as closed_at
      from chat.invite inv
      where inv.user_id = :UserId::int8 and inv.closed_at isnull
         and inv.domain_id = :DomainId::int8
      union
 
-     select 2, ch.created_at, ch.id, null::text invite_id, ch.created_at as joined_at, ch.conversation_id, ch.user_id, ch.updated_at, ch.props, ch.closed_at
+     select 2, ch.created_at, ch.id::uuid, null::uuid invite_id, ch.created_at as joined_at, ch.conversation_id, ch.user_id, ch.updated_at, ch.props, ch.closed_at
      from (
 		select ch.id, ch.created_at, ch.conversation_id, ch.user_id, ch.updated_at, ch.props, ch.closed_at
 		from chat.channel ch
@@ -50,7 +50,7 @@ from (
 		  and (ch.closed_at isnull or exists(select 1
 											 from call_center.cc_member_attempt mat
 												 inner join call_center.cc_agent a on a.id = mat.agent_id
-											 where a.user_id = :UserId::int8 and mat.agent_call_id = ch.id and mat.state != 'leaving'))
+											 where a.user_id = :UserId::int8 and mat.agent_call_id = ch.id::text and mat.state != 'leaving'))
 		  and ch.domain_id = :DomainId::int8
 		order by ch.created_at desc, ch.updated_at desc
 		limit 40
@@ -78,10 +78,10 @@ end) as "file", m.type, m.channel_id
                    ch2.type,
                    ch2.user_id,
                    ch2.name,
-                   ch2.props->>'user' as external_id	
+                   ch2.props->>'user' as external_id
             from chat.channel ch2
-            where ch2.conversation_id = c.id
-              and (not ch2.id = ch.id or ch.id isnull )
+            where ch2.conversation_id = c.id::uuid
+              and (not ch2.id::uuid = ch.id or ch.id isnull )
             limit 10
         ) t
     ) mem on true
@@ -96,7 +96,7 @@ end) as "file", m.type, m.channel_id
            a.agent_call_id as agent_channel_id,
            a.destination,
            a.state,
-		   call_center.cc_view_timestamp(a.leaving_at) as leaving_at,	
+		   call_center.cc_view_timestamp(a.leaving_at) as leaving_at,
            q.processing     as has_reporting,
 	       q.processing and q.form_schema_id notnull as has_form,
 		   q.processing_sec,
@@ -105,7 +105,7 @@ end) as "file", m.type, m.channel_id
 		   a.form_view as form
     from call_center.cc_member_attempt a
             inner join call_center.cc_queue q on q.id = a.queue_id
-        where a.agent_call_id = ch.id
+        where a.agent_call_id = ch.id::varchar
     ) at on true
 order by ch.pri, ch.updated_at desc`, map[string]interface{}{
 		"UserId":   userId,
