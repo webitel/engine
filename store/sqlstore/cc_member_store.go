abc7ac271f4a4d95b687a1b3a50ec79413d0b304
Igor Navrotskyj <navrotskyj@gmail.com>
2024-01-23 16:18:18 +0200

    DEV-3690

--- store/sqlstore/cc_member_store.go
+++ store/sqlstore/cc_member_store.go
@@ -83,6 +83,9 @@ func (s SqlMemberStore) BulkCreate(ctx context.Context, domainId, queueId int64,
 	_, err = tx.WithContext(ctx).Exec("CREATE temp table " + tableName + " ON COMMIT DROP as table call_center.cc_member with no data")
 
 	if err != nil {
+		if tx != nil {
+			tx.Rollback()
+		}
 		return nil, model.NewInternalError("store.sql_member.bulk_save.app_error", err.Error())
 	}
 
