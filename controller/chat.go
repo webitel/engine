20132135498974d814bbb0e13dd2f5d548cbf857
Dtsnko <right0346@gmail.com>
2024-11-18 19:27:27 +0200

    [WTEL-5476] +- changed premissions check on license

--- controller/chat.go
+++ controller/chat.go
@@ -7,100 +7,93 @@ import (
 	"github.com/webitel/engine/model"
 )
 
+var (
+	noChatAccessError = model.NewForbiddenError("chat.valid.license", "no CALL_CENTER license was found")
+)
+
 func (c *Controller) DeclineChat(session *auth_manager.Session, inviteId string, cause string) model.AppError {
-	permission := session.GetPermission(model.PermissionChat)
-	if !permission.CanRead() {
-		return c.app.MakePermissionError(session, permission, auth_manager.PERMISSION_ACCESS_READ)
+	if !session.HasCallCenterLicense() {
+		return noChatAccessError
 	}
 
 	return c.app.DeclineChat(session.UserId, inviteId, cause)
 }
 
 func (c *Controller) JoinChat(session *auth_manager.Session, inviteId string) (string, model.AppError) {
-	permission := session.GetPermission(model.PermissionChat)
-	if !permission.CanRead() {
-		return "", c.app.MakePermissionError(session, permission, auth_manager.PERMISSION_ACCESS_READ)
+	if !session.HasCallCenterLicense() {
+		return "", noChatAccessError
 	}
 
 	return c.app.JoinChat(session.UserId, inviteId)
 }
 
 func (c *Controller) LeaveChat(session *auth_manager.Session, channelId, conversationId string, reason model.LeaveCause) model.AppError {
-	permission := session.GetPermission(model.PermissionChat)
-	if !permission.CanRead() {
-		return c.app.MakePermissionError(session, permission, auth_manager.PERMISSION_ACCESS_READ)
+	if !session.HasCallCenterLicense() {
+		return noChatAccessError
 	}
 
 	return c.app.LeaveChat(session.UserId, channelId, conversationId, reason)
 }
 
 func (c *Controller) CloseChat(session *auth_manager.Session, channelId, conversationId string, cause model.CloseCause) model.AppError {
-	permission := session.GetPermission(model.PermissionChat)
-	if !permission.CanRead() {
-		return c.app.MakePermissionError(session, permission, auth_manager.PERMISSION_ACCESS_READ)
+	if !session.HasCallCenterLicense() {
+		return noChatAccessError
 	}
 
 	return c.app.CloseChat(session.UserId, channelId, conversationId, cause)
 }
 
 func (c *Controller) SendTextChat(session *auth_manager.Session, channelId, conversationId, text string) model.AppError {
-	permission := session.GetPermission(model.PermissionChat)
-	if !permission.CanRead() {
-		return c.app.MakePermissionError(session, permission, auth_manager.PERMISSION_ACCESS_READ)
+	if !session.HasCallCenterLicense() {
+		return noChatAccessError
 	}
 
 	return c.app.SendTextMessage(session.UserId, channelId, conversationId, text)
 }
 
 func (c *Controller) SendFileChat(session *auth_manager.Session, channelId, conversationId string, file *model.ChatFile) model.AppError {
-	permission := session.GetPermission(model.PermissionChat)
-	if !permission.CanRead() {
-		return c.app.MakePermissionError(session, permission, auth_manager.PERMISSION_ACCESS_READ)
+	if !session.HasCallCenterLicense() {
+		return noChatAccessError
 	}
 
 	return c.app.SendFileMessage(session.UserId, channelId, conversationId, file)
 }
 
 func (c *Controller) AddToChat(session *auth_manager.Session, userId int64, channelId, conversationId, title string) model.AppError {
-	permission := session.GetPermission(model.PermissionChat)
-	if !permission.CanRead() {
-		return c.app.MakePermissionError(session, permission, auth_manager.PERMISSION_ACCESS_READ)
+	if !session.HasCallCenterLicense() {
+		return noChatAccessError
 	}
 
 	return c.app.AddToChat(session.UserId, userId, channelId, conversationId, title)
 }
 
 func (c *Controller) StartChat(session *auth_manager.Session, userId int64) model.AppError {
-	permission := session.GetPermission(model.PermissionChat)
-	if !permission.CanRead() {
-		return c.app.MakePermissionError(session, permission, auth_manager.PERMISSION_ACCESS_READ)
+	if !session.HasCallCenterLicense() {
+		return noChatAccessError
 	}
 
 	return c.app.StartChat(session.DomainId, session.UserId, userId)
 }
 
 func (c *Controller) UpdateChannelChat(session *auth_manager.Session, channelId string, readUntil int64) model.AppError {
-	permission := session.GetPermission(model.PermissionChat)
-	if !permission.CanRead() {
-		return c.app.MakePermissionError(session, permission, auth_manager.PERMISSION_ACCESS_READ)
+	if !session.HasCallCenterLicense() {
+		return noChatAccessError
 	}
 
 	return c.app.UpdateChannelChat(session.UserId, channelId, readUntil)
 }
 
 func (c *Controller) ListActiveChat(ctx context.Context, session *auth_manager.Session, page, size int) ([]*model.Conversation, model.AppError) {
-	permission := session.GetPermission(model.PermissionChat)
-	if !permission.CanRead() {
-		return nil, c.app.MakePermissionError(session, permission, auth_manager.PERMISSION_ACCESS_READ)
+	if !session.HasCallCenterLicense() {
+		return nil, noChatAccessError
 	}
 
 	return c.app.ListActiveChat(ctx, session.Token, session.DomainId, session.UserId, page, size)
 }
 
 func (c *Controller) BlindTransferChat(ctx context.Context, session *auth_manager.Session, conversationId, channelId string, planId int32, vars map[string]string) model.AppError {
-	permission := session.GetPermission(model.PermissionChat)
-	if !permission.CanRead() {
-		return c.app.MakePermissionError(session, permission, auth_manager.PERMISSION_ACCESS_READ)
+	if !session.HasCallCenterLicense() {
+		return noChatAccessError
 	}
 
 	return c.app.BlindTransferChat(ctx, session.DomainId, conversationId, channelId, planId, vars)
@@ -108,18 +101,16 @@ func (c *Controller) BlindTransferChat(ctx context.Context, session *auth_manage
 
 // todo check userId in domain
 func (c *Controller) BlindTransferChatToUser(session *auth_manager.Session, conversationId, channelId string, userId int64, vars map[string]string) model.AppError {
-	permission := session.GetPermission(model.PermissionChat)
-	if !permission.CanRead() {
-		return c.app.MakePermissionError(session, permission, auth_manager.PERMISSION_ACCESS_READ)
+	if !session.HasCallCenterLicense() {
+		return noChatAccessError
 	}
 
 	return c.app.BlindTransferChatToUser(session.DomainId, conversationId, channelId, userId, vars)
 }
 
 func (c *Controller) BroadcastChatBot(session *auth_manager.Session, profileId int64, peer []string, text string) model.AppError {
-	permission := session.GetPermission(model.PermissionChat)
-	if !permission.CanRead() {
-		return c.app.MakePermissionError(session, permission, auth_manager.PERMISSION_ACCESS_READ)
+	if !session.HasCallCenterLicense() {
+		return noChatAccessError
 	}
 
 	return c.app.BroadcastChatBot(context.TODO(), session.Domain(0), profileId, peer, text)
@@ -127,10 +118,8 @@ func (c *Controller) BroadcastChatBot(session *auth_manager.Session, profileId i
 
 // todo check userId in domain
 func (c *Controller) SetContactChat(session *auth_manager.Session, channelId string, conversationId string, contactId int64) model.AppError {
-	permission := session.GetPermission(model.PermissionChat)
-	if !permission.CanRead() {
-		return c.app.MakePermissionError(session, permission, auth_manager.PERMISSION_ACCESS_READ)
+	if !session.HasCallCenterLicense() {
+		return noChatAccessError
 	}
-
 	return c.app.SetChatContactId(context.Background(), session.DomainId, session.UserId, contactId, channelId, conversationId)
 }
