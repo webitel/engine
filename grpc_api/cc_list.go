037ec1ab7cc4a64474161a94b49ee6313a104e8b
i.navrotskyj <i.navrotskyj@gmail.com>
2025-04-11 15:00:23 +0300

    fixed audit communication

--- grpc_api/cc_list.go
+++ grpc_api/cc_list.go
@@ -397,7 +397,7 @@ func (api *list) UpdateListCommunication(ctx context.Context, in *engine.UpdateL
 		return nil, err
 	}
 
-	api.app.AuditCreate(ctx, session, model.PERMISSION_SCOPE_CC_LIST, communication.ListId, communication)
+	api.app.AuditUpdate(ctx, session, model.PERMISSION_SCOPE_CC_LIST, communication.ListId, communication)
 
 	return toEngineListCommunication(communication), nil
 }
@@ -432,7 +432,7 @@ func (api *list) DeleteListCommunication(ctx context.Context, in *engine.DeleteL
 	if err != nil {
 		return nil, err
 	} else {
-		api.app.AuditCreate(ctx, session, model.PERMISSION_SCOPE_CC_LIST, communication.ListId, communication)
+		api.app.AuditDelete(ctx, session, model.PERMISSION_SCOPE_CC_LIST, communication.ListId, communication)
 		return toEngineListCommunication(communication), nil
 	}
 }
