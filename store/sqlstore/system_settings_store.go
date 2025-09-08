8e86af25c9b29fa04461f2b8cf853f5ea790ec71
Kontentski <vanya.2001.556@gmail.com>
2025-09-08 10:54:55 +0300

    WTEL-4667
    add password related system settings

--- store/sqlstore/system_settings_store.go
+++ store/sqlstore/system_settings_store.go
@@ -23,7 +23,8 @@ var (
 		model.SysNameCallEndSoundNotification, model.SysNameCallEndPushNotification, model.SysNameChatEndSoundNotification,
 		model.SysNameChatEndPushNotification, model.SysNameTaskEndSoundNotification, model.SysNameTaskEndPushNotification,
 		model.SysNamePushNotificationTimeout, model.SysNameLabelsToLimitContacts, model.SysNameAutolinkMailToContact, model.SysNameNewChatSoundNotification,
-		model.SysNameNewMessageSoundNotification, model.SysNameScreenshotInterval,
+		model.SysNameNewMessageSoundNotification, model.SysNameScreenshotInterval, model.SysNamePasswordExpiryDays,
+		model.SysNamePasswordMinLength, model.SysNamePasswordMinCategories,
 	}
 )
 
