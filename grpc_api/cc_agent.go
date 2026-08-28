2b0b4cce936f3ae81eb1146932e0d10905ba7017
suifri <illia.khvehschuk.pz.2021@lpnu.ua>
2026-08-28 13:05:13 +0300

    [WTEL-10170]refactor(cc_agent): rename activity_type to status_type

--- grpc_api/cc_agent.go
+++ grpc_api/cc_agent.go
@@ -1064,7 +1064,7 @@ func transformAgent(src *model.Agent) *engine.Agent {
 		TaskCount:             src.TaskCount,
 		ScreenControl:         src.ScreenControl,
 		AllowSetScreenControl: src.AllowSetScreenControl,
-		ActivityType:          src.ActivityType,
+		StatusType:            src.ActivityType,
 	}
 
 	if src.ProgressiveCount != nil {
