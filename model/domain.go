62f8d9a2788d6e4768f94e0626b2d5d4814cab31
suifri <illia.khvehschuk.pz.2021@lpnu.ua>
2026-09-10 12:02:02 +0300

    [WTEL-10383]feature(broker): add creating system online skill on new domain event
    - add creation of simple topic broker queue
    - add queue configuration object
    - add new domain created handler

--- model/domain.go
+++ model/domain.go
@@ -1,6 +1,44 @@
 package model
 
-import "time"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type DomainEvent struct {
+	ID int64
+}
+
+func NewDomainEventFromRoutingKey(rk string) (*DomainEvent, AppError) {
+	splitted := strings.Split(rk, ".")
+	if len(splitted) < 3 {
+		return nil, NewBadRequestError(
+			"model.domain.new_domain_event.invalid_rk_len",
+			"received roting key with len less than 3",
+		)
+	}
+
+	const domainIdIndex = 2
+
+	parsedDomainId, err := strconv.ParseInt(splitted[domainIdIndex], 10, 64)
+	if err != nil {
+		return nil, NewBadRequestError(
+			"model.domain.new_domain_event.parsing_id",
+			fmt.Sprintf("parsing routing key id to integer: %+v", err),
+		)
+	}
+
+	if parsedDomainId <= 0 {
+		return nil, NewBadRequestError(
+			"model.domain.new_domain_event.domain_id_less_or_equal_zero",
+			fmt.Sprintf("received domain id less or equal zero: %d", parsedDomainId),
+		)
+	}
+
+	return &DomainEvent{ID: parsedDomainId}, nil
+}
 
 type DomainProvider interface {
 	Domain(int64) int64
